08981460beed0202fa77d06602a4f0e60f599d94
Daniel Ferreira <danielscontaparateste@gmail.com>
2022-05-10 17:47:55 -0300

    Aula 10/05

--- DOM-Vetores/js/pacientes.js
+++ DOM-Vetores/js/pacientes.js
@@ -1,27 +1,42 @@
 const listaPacientes = [];
+const listaPrioridades = [];
 
 function addPaciente(){
-    const nomePaciente = document.querySelector('#iptNome').value;
-    listaPacientes.push(nomePaciente);
+    const inputPaciente = document.querySelector('#iptNome');
+    listaPacientes.push(inputPaciente.value);
+
+    const inputPrioridade = document.querySelector("#iptPrioridade");
+    listaPrioridades.push(inputPrioridade.checked);
 
     var html = '<ul>';
     for (let k =0; k<listaPacientes.length; k++){
-        html = html + "<li>" + listaPacientes[k] + "</li>";
+        if (listaPrioridades[k]){
+            html = html + "<li>" + listaPacientes[k] + " <span class='classPrioridade'>(Prioridade)</span>"+"</li>";
+        }else{
+            html = html + "<li>" + listaPacientes[k] + "</li>";
+        }    
     }
     html = html + "</ul>";
 
     document.querySelector('#lista').innerHTML = html;
+
+    inputPaciente.value = "";
+    inputPrioridade.checked = false;
+    // inputPrioridade.disabled = true;
 }
 
 function removerPaciente(){
 
-    /* Exercicio de casa : verificar se o vetor está 
-    vazio antes de aplicar a função para remover elemento do vetor */
     listaPacientes.shift();
+    listaPrioridades.shift();
 
     var html = '<ul>';
     for (let k =0; k<listaPacientes.length; k++){
-        html = html + "<li>" + listaPacientes[k] + "</li>";
+        if (listaPrioridades[k]){
+            html = html + "<li>" + listaPacientes[k] + " <span class='classPrioridade'>(Prioridade)</span>"+"</li>";
+        }else{
+            html = html + "<li>" + listaPacientes[k] + "</li>";
+        }    
     }
     html = html + "</ul>";
 
