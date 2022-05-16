09db9344fddbd7b4d0727f200581b088dfdc21ce
Daniel Ferreira <danielscontaparateste@gmail.com>
2022-05-16 17:28:07 -0300

    Aula 16/05

--- DOM-CreateElement/js/script.js
+++ DOM-CreateElement/js/script.js
@@ -12,15 +12,23 @@ function criarBlocos(){
     const inputQuantidade = document.querySelector("#iptQuantidade");
     const qtd = inputQuantidade.value;
     
-    for (k = 0; k<qtd; k++){
-        const bloco = document.createElement("div");
-        bloco.classList.add('box');
-        bloco.innerHTML = "<span class='infoBloco'>"+k+"</span>";
-        // Anexar ao body
-        // document.body.appendChild(bloco);
+    for (let c=0; c<qtd; c++){
+        
+        for (let k = 0; k<qtd; k++){
+            const bloco = document.createElement("div");
+            bloco.classList.add('box');
+            bloco.innerHTML = "<span class='infoBloco'>"+k+"</span>";
+            // bloco.value = k;
+            // bloco.setAttribute("id", k);
+            bloco.onclick = function (){alert("Olá mundo")};
+            
+            const jogo = document.querySelector("#jogo");
+            jogo.appendChild(bloco);
+        }    
+        // const quebraLinha = document.createElement("br");
+        // jogo.appendChild(quebraLinha);
 
-        const body = document.querySelector("body");
-        body.appendChild(bloco);
-    }    
+    }
 
 }
+
