cca611133881ef4b200db1a2ffa4c1370f1ee8a0
Daniel Ferreira <danielscontaparateste@gmail.com>
2022-06-15 17:31:46 -0300

    Aula 15/06

--- Exercicio_Urna/js/script.js
+++ Exercicio_Urna/js/script.js
@@ -3,10 +3,15 @@ var candidatos = ['Jessica','Joao Victor','Ismael'];
 var votosCands = [0,0,0];
  
 function votar(){
+
+    const tempo = 2;
     
     // Desabilitar o botao de votar
     const btnVotar = document.querySelector("#btnVotar");
     btnVotar.disabled = true;
+
+    const btnResultado = document.querySelector("#btnResultado");
+    btnResultado.disabled = true;
 
     const iptCands = document.getElementsByName("candidatos");
     let seVoto = false;
@@ -21,15 +26,20 @@ function votar(){
     if (seVoto){
         votosCands[k] = votosCands[k]+1;    
         
+        contadorRegressivo(tempo, true);
+
         spnMsg.innerHTML = "Obrigado pelo seu voto. ";
         setTimeout(function () {
             spnMsg.innerHTML = "";
             iptCands[k].checked = false;
-            btnVotar.disabled = false;   
-        }, 2000);    
+            btnVotar.disabled = false;  
+            btnResultado.disabled = false; 
+            contadorRegressivo(tempo, false);
+        }, tempo * 1000);    
     }else{
         spnMsg.innerHTML = "Por favor, selecione o seu candidato. ";
         btnVotar.disabled = false;   
+        btnResultado.disabled = false;
     }
 }
 
@@ -53,6 +63,8 @@ function resultado(){
     const spnMsg = document.getElementById("msg");
     spnMsg.innerHTML = msg;
 
+    reiniciar();
+
 }
 
 function getVencedor(){
@@ -73,3 +85,39 @@ function getVencedor(){
     
 
 }
+
+function reiniciar(){
+    const btnVotar = document.querySelector("#btnVotar");
+    btnVotar.innerHTML = "Reiniciar";
+    
+    btnVotar.onclick = function () {
+        alert("Essa urna será reiniciada. ");
+        // window.location.reload();
+        votosCands = [0,0,0];
+        const spnMsg = document.getElementById("msg");
+        spnMsg.innerHTML = "";
+        this.onclick = "";
+        this.addEventListener("click", votar);
+        this.innerHTML = "Votar";
+    } ;
+
+
+    btnVotar.disabled = false;
+
+}
+
+function contadorRegressivo(tempo, mostrar){
+    
+    const spnRegressivo = document.querySelector("#regressivo");
+
+    if (mostrar){  
+        spnRegressivo.innerHTML = tempo;  
+        stReg = setInterval(function () {
+            tempo = tempo - 1;
+            spnRegressivo.innerHTML = tempo
+        }, 1000);
+    }else{
+        clearInterval(stReg);
+        spnRegressivo.innerHTML = "";
+    }    
+}   
